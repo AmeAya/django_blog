7a20a1edbd6d5bf44bc9765ecd160e430ed9420f
Aigerim <aigerim@MacBook-Air-Aigerim.local>
2022-05-11 20:14:30 +0600

    login form

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('blog.urls')),
 ]
+
+# 127.0.0.1/accounts/
