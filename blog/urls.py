4221692abcc90332f6227bc31aacd7fcc5aaaab3
AmeAmeAya <ameameameaya@gmail.com>
2022-05-23 20:27:10 +0600

    qwe

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import blogListView, blogDetailView
+from .views import blogListView, blogDetailView, blogCreateView
 
 urlpatterns = [
+    path('post/new', blogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', blogDetailView.as_view(), name='post_detail'),
     path('', blogListView.as_view(), name='home'),
 ]
